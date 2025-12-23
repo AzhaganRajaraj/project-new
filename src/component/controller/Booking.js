6d612a157683eedc8892414ecadedb9efac572d7
/azhagan <azhagana686@gmail.com>
2025-12-23 11:41:02 +0530

    new

--- src/component/controller/Booking.js
+++ src/component/controller/Booking.js
@@ -1,13 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Booking() {
     const[booking, setBooking] = useState({
-        userId: "",
-        scheduleId: "",
-        customerId: "",
-        totalFare: "",
+    
+        schedule: "",
+        customer: "",
+        totalfare: "",
+        status: "",
         bookingTime: ""
     });
+
+    const[customer, setCustomer] = useState([]);
+    const[schedule, setSchedule] = useState([]);
+
+    const router = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        fetch("http://localhost:9000/api/customers/", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }).then(res => res.json())
+           .then(data => {
+            console.log(data)
+            setCustomer(data)
+           })
+    },[])
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        fetch("http://localhost:9000/api/locations/", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }).then(res => res.json())
+          .then(data => {
+            console.log(data)
+            setSchedule(data)
+          })
+    },[])
 
     function handleChange(e) {
         setBooking({ ...booking, [e.target.name]: e.target.value});
@@ -19,33 +54,56 @@ export default function Booking() {
         fetch("http://localhost:9000/api/bookings/", {
             method: "POST",
             headers: {
-                "content-type":"application/json",
+                "content-Type":"application/json",
                 "Authoruzation":`Bearer ${token}`
             },
             body:JSON.stringify(booking)
         })
           .then(data => {
             console.log(data)
-          })
+            router("/payments")
+          }).catch(err => console.log(err))
     }
 
     return (
         <div className="container">
             <h3 className="text-center">Booking</h3>
             <form onSubmit={handleSubmit} >
-                <div className="mb-3">
-                    <label htmlFor="userId" className="form-label">User ID</label>
-                    <input type="text" className="form-control" id="userId" name="userId" placeholder="Enter user Id" onChange={handleChange} />
-                </div>
+
 
                 <div className="mb-3">
-                    <label htmlFor="scheduleId" className="form-label">Schedule ID</label>
-                    <input type="text" className="form-control" id="scheduleId" name="scheduleId" placeholder="Enter schedule Id" onChange={handleChange} />
+                    <label htmlFor="customer" className="form-label">Customer</label>
+                    <select className="form-control" id="customer" name="customer" onChange={handleChange}>
+                           <option>Choose Customer Name</option>
+                           {
+                            customer.map((val) => 
+                            <option value={val.id}>{val.user?.fullName}</option> )
+                           }
+                           </select>
+                </div>
+                
+                <div className="mb-3">
+                    <label htmlFor="schedule" className="form-label">Schedule</label>
+                    <select className="form-control" id="schedule" name="schedule" onChange={handleChange}>
+                        <option>Destination</option>
+                        {
+                            schedule.map((val) => 
+                                <option value={val.id}>{val.city} to {val.state}</option>
+                            )
+                        }
+                    </select>
                 </div>
 
+
                 <div className="mb-3">
-                    <label htmlFor="customerId" className="form-label">Customer ID</label>
-                    <input type="text" className="form-control" id="customerId" name="customerId" placeholder="Enter customer Id" onChange={handleChange} />
+                    <label htmlFor="status" className="form-label">status</label>
+                    <select className="form-control" id="status" name="status" onChange={handleChange}>
+                        <option value="">Payment Status</option>
+                        <option value="SUCCESS">Success</option>
+                        <option value="PENDING">Pending</option>
+                        <option value="FAILED">Failed</option>
+                        <option value="REFUNDED">Refunded</option>
+                    </select>
                 </div>
     
                 <div className="mb-3">
@@ -54,8 +112,8 @@ export default function Booking() {
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="totalFare" className="form-label">Total Fare</label>
-                    <input type="number" className="form-control" id="totalFare" name="totalFare" placeholder="Enter total fare" onChange={handleChange} />
+                    <label htmlFor="totalfare" className="form-label">Total Fare</label>
+                    <input type="text" className="form-control" id="totalfare" name="totalfare" placeholder="Enter total fare" onChange={handleChange} />
                 </div>
 
                 <button type="submit" className="btn btn-success">Submit</button>
--- src/component/controller/Payment.js
+++ src/component/controller/Payment.js
@@ -1,29 +1,52 @@
-import { useState } from "react";
+import {  useEffect, useState } from "react";
 
 export default function Payment() {
     const[payment, setPayment] = useState({
         bookingId: "",
         amount: "",
-        paidTime: "",
+         paidTime: "",
         transactionId: "",
-        status: "",
-        customerId: ""
+        status: ""
 
     });
+
+    const[booking, setBooking] = useState([]);
+
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+        fetch("http://localhost:9000/api/bookings/", {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }).then(res => res.json())
+          .then(data => {
+            console.log(data)
+            setBooking(data)
+          })
+          .catch(err => console.log(err))
+    },[])
+
 
     function handleChange(e) {
         setPayment({ ...payment, [e.target.name]: e.target.value});
     }
 
     function handleSubmit(e) {
+        const token = localStorage.getItem("token");
         e.preventDefault();
         fetch("http://localhost:9000/api/payments/", {
             method: "POST",
             headers: {
-                "content-type":"application/json"
+                "content-type":"application/json",
+                "Authorization": `Bearer ${token}`
             },
             body:JSON.stringify(payment)
         })
+        .then(data => {
+            console.log(data)
+        }).catch(err => console.log(err))
     }
 
     return (
@@ -32,32 +55,29 @@ export default function Payment() {
             <form onSubmit={handleSubmit} >
                 <div className="mb-3">
                     <label htmlFor="bookingId" className="form-label">Booking ID</label>
-                    <input type="text" className="form-control" id="bookingId" name="bookingId" placeholder="Enter booking id" onChange={handleChange} />
+                    <select className="form-control" id="bookingId" name="bookingId"onChange={handleChange}>
+                        <option>Choose ID</option>
+                        {
+                            booking.map((val) =>
+                                  <option value={val.id}>{val.id}</option>)
+                        }
+                    </select>
                 </div>
 
-
                 <div className="mb-3">
-                    <label htmlFor="amount" className="form-label">Paid Time</label>
-                    <input type="text" className="form-control" id="amount" name="amount" placeholder="Enter amount" onChange={handleChange} />
+                    <label htmlFor="transactionId" className="form-label">transaction ID</label>
+                    <input type="text" className="form-control" id="transactionId" name="transactionId" placeholder="Enter transactionId" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="amount" className="form-label">transaction ID</label>
-                    <input type="text" className="form-control" id="amount" name="amount" placeholder="Enter amount" onChange={handleChange} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="amount" className="form-label">Customer ID</label>
-                    <input type="text" className="form-control" id="amount" name="amount" placeholder="Enter amount" onChange={handleChange} />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="amount" className="form-label">Status</label>
-                    <input type="text" className="form-control" id="amount" name="amount" placeholder="Enter amount" onChange={handleChange} />
+                    <label htmlFor="status" className="form-label">Status</label>
+                    <input type="text" className="form-control" id="status" name="status" placeholder="Status" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="amount" className="form-label">Amount</label>
                     <input type="text" className="form-control" id="amount" name="amount" placeholder="Enter amount" onChange={handleChange} />
                 </div>
 
-                <button type="submit" className="btn btn-success">Pay</button>
+                <button type="submit" className="btn btn-success">Save</button>
             </form>
         </div>
     )
